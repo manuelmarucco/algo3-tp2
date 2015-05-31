164ccaffa81423717cb13b5dc6bbc41aa052b505
jgalloni <jgalloni91@gmail.com>
2015-05-31 00:57:22 -0300

    removi linea vacia

--- src/fiuba/algo3/algocraft/comandos/AccionRegenerar.java
+++ src/fiuba/algo3/algocraft/comandos/AccionRegenerar.java
@@ -5,7 +5,6 @@ import fiuba.algo3.algocraft.unidades.RazaDeUnidad;
 public class AccionRegenerar extends AccionesDeRazaDeUnidad  {
     @Override
     public void actuar(RazaDeUnidad unidad) {
-
         unidad.recibirDanio(-unidad.getAtributos().get("regeneracion"));//esto es medio choto pero tengo q pensar una mejor forma de hacerlo
     }
 }
