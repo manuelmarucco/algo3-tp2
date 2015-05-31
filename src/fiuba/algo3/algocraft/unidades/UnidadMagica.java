670b4abdded94ec9e3a863478256c2a9cf60e788
jgalloni <jgalloni91@gmail.com>
2015-05-31 03:02:10 -0300

    agregue energia a las unidades magicas

--- src/fiuba/algo3/algocraft/unidades/UnidadMagica.java
+++ src/fiuba/algo3/algocraft/unidades/UnidadMagica.java
@@ -1,4 +1,18 @@
 package fiuba.algo3.algocraft.unidades;
 
 public class UnidadMagica extends ClaseDeUnidad {
+    private final int regeneracionEnergia;
+    private final int energiaTotal;
+    private int energiaActual;
+
+    public UnidadMagica(int energiaTotal, int energiaInicial, int regeneracionEnergia) {
+        super();
+        this.energiaTotal = energiaTotal;
+        this.energiaActual = energiaInicial;
+        this.regeneracionEnergia = regeneracionEnergia;
+    }
+
+    public void regenerarEnergia() {
+        this.energiaActual = Math.min(energiaTotal, energiaActual + regeneracionEnergia);
+    }
 }
