6e252f708a82b2082fe5a9fe8965b41f4a7646af
jgalloni <jgalloni91@gmail.com>
2015-05-31 02:31:49 -0300

    renombre agresor a actor para ser mas generico

--- src/fiuba/algo3/algocraft/comandos/AccionAtacar.java
+++ src/fiuba/algo3/algocraft/comandos/AccionAtacar.java
@@ -4,7 +4,7 @@ import fiuba.algo3.algocraft.unidades.Unidad;
 
 public class AccionAtacar extends AccionesDeClasedeUnidad {
     @Override
-    public void actuar(Unidad objetivo,Unidad agresor) {
-        objetivo.recibirDanio(agresor);
+    public void actuar(Unidad objetivo, Unidad actor) {
+        objetivo.recibirDanio(actor);
     }
 }
--- src/fiuba/algo3/algocraft/comandos/AccionesDeClasedeUnidad.java
+++ src/fiuba/algo3/algocraft/comandos/AccionesDeClasedeUnidad.java
@@ -3,5 +3,5 @@ package fiuba.algo3.algocraft.comandos;
 import fiuba.algo3.algocraft.unidades.Unidad;
 
 public abstract class AccionesDeClasedeUnidad {
-    public abstract void actuar(Unidad objetivo,Unidad agresor);
+    public abstract void actuar(Unidad objetivo,Unidad actor);
 }
