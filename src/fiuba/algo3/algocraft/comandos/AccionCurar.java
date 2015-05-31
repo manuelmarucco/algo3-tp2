e19f613082011c5c48417606d7d59556b4fd4a34
jgalloni <jgalloni91@gmail.com>
2015-05-31 02:32:24 -0300

    cree AccionCurar

---
+++ src/fiuba/algo3/algocraft/comandos/AccionCurar.java
@@ -0,0 +1,11 @@
+package fiuba.algo3.algocraft.comandos;
+
+import fiuba.algo3.algocraft.unidades.Unidad;
+
+public class AccionCurar extends AccionesDeClasedeUnidad {
+    @Override
+    public void actuar(Unidad objetivo, Unidad actor) {
+        // TODO: Ver como le saco a actor la cantidad a regenerar al objetivo.
+        objetivo.getVida().regenerar(0);
+    }
+}
