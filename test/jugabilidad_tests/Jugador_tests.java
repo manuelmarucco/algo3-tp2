0d4f5ecffe50c381bf4acfecc1af75a35123ff49
manuelmarucco <manuelmarucco@gmail.com>
2015-06-01 13:39:16 -0300

    agrego el test de recoleccion fallando. hay q agregar el codigo para q pase

--- test/jugabilidad_tests/Jugador_tests.java
+++ test/jugabilidad_tests/Jugador_tests.java
@@ -1,7 +1,31 @@
 package jugabilidad_tests;
 
+import org.junit.Assert;
+import org.junit.Test;
+
+import construcciones.comandos.ConstruccionesDisponibles;
+import auxiliares.Recursos;
+import razas.Terran;
+import jugabilidad.Jugador;
+
 public class Jugador_tests {
 
-	
+	@Test
+	public void JugadorRecolectaRecursos(){
+		Jugador jugador1 = new Jugador(new Terran(),new Recursos(400,0));
+		
+		jugador1.construir(ConstruccionesDisponibles.CENTRODEMINERAL); //+10 mineral
+		jugador1.construir(ConstruccionesDisponibles.CENTRODEMINERAL);; //+10 mineral
+		jugador1.construir(ConstruccionesDisponibles.REFINERIA); //+10 gas
+
+		int mineralesAnterior = jugador1.getRecursos().getMinerales();
+		int gasAnterior = jugador1.getRecursos().getGasVespeno();
+		
+		jugador1.recolectar();
+		
+				
+		Assert.assertEquals(20 + mineralesAnterior,jugador1.getRecursos().getMinerales());
+		Assert.assertEquals(10 + mineralesAnterior,jugador1.getRecursos().getGasVespeno());
+	}
 
 }
