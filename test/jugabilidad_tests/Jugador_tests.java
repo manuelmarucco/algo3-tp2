a2a7c2a1701dce4c42d9a2a3857a65212d3f7e36
jgalloni <jgalloni91@gmail.com>
2015-06-02 00:33:02 -0300

    ya pasa el test JugadorCrearUnaBarracaYUnMarine (faltaba crear el depositos de suministros)

--- test/jugabilidad_tests/Jugador_tests.java
+++ test/jugabilidad_tests/Jugador_tests.java
@@ -25,7 +25,8 @@ public class Jugador_tests {
 	@Test
 	public void JugadorCreaUnaBarracaYUnMarine(){
 		Jugador jugador1 = new Jugador(new Terran(),new Recursos(400,0));
-		
+
+		jugador1.construir(ConstruccionesDisponibles.DEPOSITODESUMINISTROS);
 		jugador1.construir(ConstruccionesDisponibles.BARRACA);
 		jugador1.buscarConstruccionCreada("Barraca").accionesDeEdificio(jugador1, AccionesDisponibles.EntrenarMarine);
 		
