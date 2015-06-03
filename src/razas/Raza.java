e5eebae86fe4dcc36f1d3f8f34391fc6b150778c
manuelmarucco <manuelmarucco@gmail.com>
2015-06-03 01:18:47 -0300

    borré comentarios que no se usan

--- src/razas/Raza.java
+++ src/razas/Raza.java
@@ -10,17 +10,6 @@ public class Raza {
 	
 	protected HashMap<ConstruccionesDisponibles,AccionConstruir> comandos;
 	
-	/*
-	public HashMap<ConstruccionesDisponibles,AccionConstruir> getComandos() {
-		return comandos;
-	}
-	*/
-	/*
-	public void setComandos(HashMap<ConstruccionesDisponibles,AccionConstruir> comandos) {
-		this.comandos = comandos;
-	}
-	*/
-	
 	public AccionConstruir seleccionarConstruccion(ConstruccionesDisponibles construccion){
 		return  comandos.get(construccion);
 	}
