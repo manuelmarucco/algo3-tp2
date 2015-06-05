baa5af1d94bd77ede86f2435928116009c738728
manuelmarucco <manuelmarucco@gmail.com>
2015-06-05 14:48:57 -0300

    agrego el entrenamiento del golliat y su test

--- test/tests_de_integracion/EntrenamientoUnidadesTerranTest.java
+++ test/tests_de_integracion/EntrenamientoUnidadesTerranTest.java
@@ -1,11 +1,13 @@
 package tests_de_integracion;
 
 import construcciones.terran.Barraca;
+import construcciones.terran.Fabrica;
 import jugabilidad.Jugador;
 import jugabilidad.auxiliares.Recursos;
 import org.junit.Assert;
 import org.junit.Test;
 import razas.Terran;
+import unidades.terrran.Golliat;
 import unidades.terrran.Marine;
 
 public class EntrenamientoUnidadesTerranTest {
@@ -24,5 +26,24 @@ public class EntrenamientoUnidadesTerranTest {
 		
 		Assert.assertTrue(j.buscarUnidad(m));
 	}
+
+	@Test
+	public void entrenoGolliatYSeAgregaAlJugador() {
+		Jugador j = new Jugador(new Terran(),new Recursos(1000,1000));
+		Barraca b = new Barraca(j);
+		Fabrica f = new Fabrica(j);
+		Golliat g = new Golliat();
+
+		j.construir(b);
+		for(int i = 0; i<12; i++) j.update();
+		j.construir(f);
+		for(int i = 0; i<12; i++) j.update();
+
+		f.entrenarGolliat(g);
+		for(int i = 0; i<6; i++) j.update();
+
+		Assert.assertTrue(j.buscarUnidad(g));
+	}
+
 
 }
