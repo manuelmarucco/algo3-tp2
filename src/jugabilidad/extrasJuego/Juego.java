29e0160f713608b52c033c244dc599ea3703eece
Mariano <mariano_cinalli@hotmail.com>
2015-06-09 16:45:28 -0300

    Modifico Juego, para que use el creador de jugador

--- src/jugabilidad/extrasJuego/Juego.java
+++ src/jugabilidad/extrasJuego/Juego.java
@@ -1,23 +1,51 @@
 package jugabilidad.extrasJuego;
 
+import excepciones.ExcepcionNoSePudoCrearElJugador;
 import interfaces.Actualizable;
 import jugabilidad.Jugador;
+import jugabilidad.RazaDeJugador.JugadorProtoss;
+import jugabilidad.RazaDeJugador.JugadorTerran;
 
 
 public class Juego implements Actualizable{
+
     AdministradorDeTurnos administradorDeTurnos;
 
     public Jugador getJugador(){
        return administradorDeTurnos.getJugadorDelTurnoActual();
     }
-/*
+
     public void crearJugadorProtoss(String nombre, String color){
-        JugadorProtoss j = new JugadorProtoss(nombre,color);
-         administradorDeTurnos.agregarJugador(j);
+
+        CreadorDeJugador creadorDeJugador = new CreadorDeJugador();
+        JugadorProtoss jugador = null;
+
+        try {
+            jugador = creadorDeJugador.crearNuevoJugadorProtos(nombre,color);
+        } catch (ExcepcionNoSePudoCrearElJugador e) {
+            e.printStackTrace();
+        }
+
+        administradorDeTurnos.agregarJugador(jugador);
     }
-*/
+
+    public void crearJugadorTerran(String nombre, String color){
+
+        CreadorDeJugador creadorDeJugador = new CreadorDeJugador();
+        JugadorTerran jugador = null;
+
+        try {
+            jugador = creadorDeJugador.crearNuevoJugadorTerran(nombre, color);
+        } catch (ExcepcionNoSePudoCrearElJugador e) {
+            e.printStackTrace();
+        }
+
+        administradorDeTurnos.agregarJugador(jugador);
+    }
+
     @Override
     public void update() {
         administradorDeTurnos.update();
     }
+
 }
