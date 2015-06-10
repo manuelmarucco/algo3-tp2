506f784653a57f952996ff337b7ca00ac14904a6
Mariano <mariano_cinalli@hotmail.com>
2015-06-09 23:12:13 -0300

    Subo juego, no esta terminada.

--- src/jugabilidad/extrasJuego/Juego.java
+++ src/jugabilidad/extrasJuego/Juego.java
@@ -3,13 +3,21 @@ package jugabilidad.extrasJuego;
 import excepciones.ExcepcionNoSePudoCrearElJugador;
 import interfaces.Actualizable;
 import jugabilidad.Jugador;
+import jugabilidad.ProxyMapa;
 import jugabilidad.RazaDeJugador.JugadorProtoss;
 import jugabilidad.RazaDeJugador.JugadorTerran;
+import jugabilidad.utilidadesMapa.Coordenadas;
+
+import java.util.ArrayList;
 
 
 public class Juego implements Actualizable{
 
     AdministradorDeTurnos administradorDeTurnos = new AdministradorDeTurnos();
+    CreadorDeMapa creadorDeMapa = new CreadorDeMapa();
+    ArrayList<Coordenadas> bases;
+
+    // Metodos
 
     public Jugador getJugador(){
        return administradorDeTurnos.getJugadorDelTurnoActual();
@@ -45,6 +53,17 @@ public class Juego implements Actualizable{
 
         administradorDeTurnos.agregarJugador(jugador);
         return jugador;
+    }
+
+    // No terminado.
+    public ProxyMapa crearMapa(){
+
+        ProxyMapa proxyMapa = this.creadorDeMapa.crearMapa();
+
+        this.bases = creadorDeMapa.obtenerCoordenadasDeLasBases();
+
+        return (proxyMapa);
+
     }
 
     @Override
