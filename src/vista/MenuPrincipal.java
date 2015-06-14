f314022f1089ac80d2e3e652ff83cc1390994549
manuelmarucco <manuelmarucco@gmail.com>
2015-06-14 16:28:25 -0300

    subo mas parte del menu principal

--- src/vista/MenuPrincipal.java
+++ src/vista/MenuPrincipal.java
@@ -10,12 +10,23 @@ public class MenuPrincipal extends JFrame {
 
     private JPanel background;
     private ImagePanel imageBackground;
+    private JButton button1;
+    private JButton button2;
+    private JButton crearJugadorBoton;
 
     public MenuPrincipal(){
         this.init();
     }
 
     private void init() {
+        //configuracion del boton de Crear Jugador
+        crearJugadorBoton.setIcon(new ImageIcon("images/boton.png"));
+        crearJugadorBoton.setMargin(new Insets(0, 0, 0, 0));
+        crearJugadorBoton.setBorder(null);
+        crearJugadorBoton.setVerticalTextPosition(SwingConstants.BOTTOM);
+        crearJugadorBoton.setHorizontalTextPosition(SwingConstants.CENTER);
+
+        //configuracion del fondo de pantalla
         this.add(background);
     }
 
