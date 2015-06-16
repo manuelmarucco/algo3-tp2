dcc249eec1e224a02abe41f75bda925a3ec0b150
Mariano <mariano_cinalli@hotmail.com>
2015-06-16 20:21:09 -0300

    Cambios en displayRecursos

--- src/vista/auxiliares/jugador/DisplayRecursos.java
+++ src/vista/auxiliares/jugador/DisplayRecursos.java
@@ -2,8 +2,8 @@ package vista.auxiliares.jugador;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
-import java.io.FileInputStream;
 import java.io.IOException;
 
 public class DisplayRecursos extends JFrame {
@@ -31,7 +31,7 @@ public class DisplayRecursos extends JFrame {
 
     private void init(){
 
-        this.panel = new JPanel();
+        this.panel = new JPanel( new GridLayout(1,3,0,3));
         this.cargarImagenes();
 
         this.agregarIndicadores();
@@ -44,9 +44,9 @@ public class DisplayRecursos extends JFrame {
         try{
 
             this.imagenes = new BufferedImage[3];
-            this.imagenes[0] = ImageIO.read(getClass().getResource("cristal.jpg"));
-            //this.imagenes[1] = ImageIO.read(getClass().getResource("resourses/displayRecursos/volcan.jpg"));
-            //this.imagenes[2] = ImageIO.read(getClass().getResource("resourses/displayRecursos/man.jpg"));
+            this.imagenes[0] = ImageIO.read(getClass().getResource("imagenesRecursos/cristal.jpg"));
+            this.imagenes[1] = ImageIO.read(getClass().getResource("imagenesRecursos/volcan.jpg"));
+            this.imagenes[2] = ImageIO.read(getClass().getResource("imagenesRecursos/man.jpg"));
 
         } catch ( IOException e){
             e.printStackTrace();
@@ -56,10 +56,33 @@ public class DisplayRecursos extends JFrame {
 
     private void agregarIndicadores(){
 
-        JLabel imagenCristal = new JLabel(new ImageIcon( imagenes[0] ) );
-        imagenCristal.setText("Cristal");
-        imagenCristal.setSize(20,100);
-        this.panel.add(imagenCristal);
+        this.agregarIndicadorMinerales();
+        this.agregarIndicadorGas();
+        this.agregarIndicadorSuministros();
+
+    }
+
+    private void agregarIndicadorMinerales(){
+
+        JLabel cristal = new JLabel(new ImageIcon( imagenes[0] ) );
+        cristal.setText("Cristal: <Cantidad>");
+        this.panel.add(cristal);
+
+    }
+
+    private void agregarIndicadorGas(){
+
+        JLabel gasVespeno = new JLabel(new ImageIcon( imagenes[1] ) );
+        gasVespeno.setText("Gas vespeno: <Cantidad>");
+        this.panel.add(gasVespeno);
+
+    }
+
+    private void agregarIndicadorSuministros(){
+
+        JLabel suministros = new JLabel(new ImageIcon( imagenes[2] ) );
+        suministros.setText("Suministros: <Usado>/<Disponible>");
+        this.panel.add(suministros);
 
     }
 
