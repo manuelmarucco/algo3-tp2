f3949f7ea4f150d17bae89c7e0c94491cd8393f2
Mariano <mariano_cinalli@hotmail.com>
2015-06-17 23:22:42 -0300

    Modifico el ProxiDeHechizos para que use el nuevo metodo de mapa.

--- src/unidades/ProxyDeHechizos.java
+++ src/unidades/ProxyDeHechizos.java
@@ -1,12 +1,16 @@
 package unidades;
 
+import excepciones.Mapa.ExcepcionNoSePudoAgregarAlMapa;
 import excepciones.Unidades.ExcepcionObjetivoFueraDeRango;
 import interfaces.Cargable;
+import interfaces.ColocableEnMapa;
 import interfaces.Hechizable;
 import jugabilidad.Jugador;
 import jugabilidad.ProxyMapa;
 import jugabilidad.utilidadesMapa.Coordenadas;
 import unidades.terrran.NaveCiencia;
+
+import java.util.ArrayList;
 
 public class ProxyDeHechizos {
     private static Jugador jugador1;
@@ -20,6 +24,7 @@ public class ProxyDeHechizos {
     public static void EMP(NaveCiencia naveCiencia,Coordenadas coordenada) throws ExcepcionObjetivoFueraDeRango {
         ProxyMapa mapa = ProxyMapa.getInstance();
         if(naveCiencia.getVision()<coordenada.distancia(mapa.getCoordenada(naveCiencia))) throw new ExcepcionObjetivoFueraDeRango();
+        /*
         for(int i =-1;i<2;i++){
             for(int j =-1;j<2;j++){
 
@@ -34,11 +39,31 @@ public class ProxyDeHechizos {
                     naveCiencia.EMP(objetivoAereo);
             }
         }
+        */
+
+        // Obtengo todas las unidades alcanzables por el EMP.
+        ArrayList<ColocableEnMapa> unidadesYConstruccionesDentroDeRango = new ArrayList<>();
+
+        try {
+            unidadesYConstruccionesDentroDeRango = mapa.obtenerUnidadesYConstruccionesEncerradasEnCircunferenciaDe(coordenada, 2);
+        } catch (ExcepcionNoSePudoAgregarAlMapa e) {
+            e.printStackTrace();
+        }
+
+        // Les aplico el EMP.
+        for(ColocableEnMapa unidadOConstruccion : unidadesYConstruccionesDentroDeRango ){
+
+            Hechizable objetivo = ((Hechizable) unidadOConstruccion);
+            naveCiencia.EMP(objetivo);
+        }
+
     }
 
+    @SuppressWarnings("all")
     public static boolean esEnemigo(Unidad unidadTransporte, Cargable unidad) {
         if(jugador2.buscarUnidad(unidadTransporte)&&jugador2.buscarUnidad(unidad)) return false;
         if(jugador1.buscarUnidad(unidadTransporte)&&jugador1.buscarUnidad(unidad)) return false;
+
         return true;
     }
 }
