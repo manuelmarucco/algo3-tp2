e6b7da729425f82cb110c35c97c4230eb0297103
manuelmarucco <manuelmarucco@gmail.com>
2015-06-18 21:08:05 -0300

    subo las imagenes de todos los edificios y unidades

--- src/vista/auxiliares/jugador/DisplayNotificaciones.java
+++ src/vista/auxiliares/jugador/DisplayNotificaciones.java
@@ -17,7 +17,18 @@ public class DisplayNotificaciones extends JPanel {
         this.setSize(new Dimension(200,100));
     }
 
-    public void mostrarNotificacion(){
+    public void mostrarNotificacion(Exception e){
 
+    }
+
+    private static BufferedImage cambiarTamanio(BufferedImage img, int newW, int newH) {
+        Image tmp = img.getScaledInstance(newW, newH, Image.SCALE_SMOOTH);
+        BufferedImage dimg = new BufferedImage(newW, newH, BufferedImage.TYPE_INT_ARGB);
+
+        Graphics2D g2d = dimg.createGraphics();
+        g2d.drawImage(tmp, 0, 0, null);
+        g2d.dispose();
+
+        return dimg;
     }
 }
