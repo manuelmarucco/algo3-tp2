b5ea9ef4f4d30f8fb8938c79eb90004a5e4aeaa9
Mariano <mariano_cinalli@hotmail.com>
2015-07-01 15:12:19 -0300

    Cambios a la ventana ganador

--- src/vista/ventanaJugadores/VentanaGanador.java
+++ src/vista/ventanaJugadores/VentanaGanador.java
@@ -1,6 +1,5 @@
 package vista.ventanaJugadores;
 
-
 import vista.Actions.accionesJuego.ActionTerminarJuego;
 import vista.VentanaJuego;
 import vista.auxiliares.ImagePanel;
@@ -12,8 +11,7 @@ import java.util.Map;
 
 public class VentanaGanador extends JFrame {
 
-
-    private JLayeredPane contenedor;
+    private JPanel contenedor;
 
     public VentanaGanador(VentanaJuego ventanaJuego,String nombreGanador) {
 
@@ -24,7 +22,7 @@ public class VentanaGanador extends JFrame {
 
     private void init(String nombreGanador) {
 
-        this.contenedor = new JLayeredPane();
+        this.contenedor = new JPanel();
 
         this.armarComponentes(nombreGanador);
         this.configurarVentana();
@@ -36,31 +34,41 @@ public class VentanaGanador extends JFrame {
 
     private void armarComponentes(String nombreGanador) {
 
-        ImagePanel imageBackground = new ImagePanel("images/menu/panelGanador.jpg", 657, 364);
-
-        JPanel panel = new JPanel();
-        panel.setLayout(new BoxLayout(panel,BoxLayout.Y_AXIS));
+        JPanel panel =  new JPanel();
+        panel.setLayout(new GridLayout(4,1,0,30));
+        panel.setMaximumSize(new Dimension(657, 364));
+        panel.setAlignmentY(50);
         panel.setOpaque(false);
 
         JLabel nombre = new JLabel();
-        this.setearFuenteDeJLabel(nombre, false, Font.BOLD, 45);
-        nombre.setText(nombreGanador);
+        this.setearFuenteDeJLabel(nombre, false, Font.BOLD, 40);
+        nombre.setText("     Felicidades: " + nombreGanador);
         panel.add(nombre);
 
+        JLabel infoGanador = new JLabel();
+        this.setearFuenteDeJLabel(infoGanador, false, Font.BOLD, 40);
+        infoGanador.setText("Por ser el ganador de la partida.");
+        panel.add(infoGanador);
+
+        JLabel agradecimiento = new JLabel();
+        this.setearFuenteDeJLabel(agradecimiento, false, Font.BOLD, 40);
+        agradecimiento.setText("Gracias por jugar Algocraft");
+        panel.add(agradecimiento);
+
         JButton botonSalir = new JButton();
+        botonSalir.setSize(new Dimension(600, 30));
         botonSalir.setText("Salir");
         botonSalir.addActionListener(new ActionTerminarJuego(this));
         panel.add(botonSalir);
 
-        //this.contenedor.add(imageBackground); // Esta se agrega
-        this.contenedor.add(panel); // No se agrega
+        this.contenedor.add(panel);
 
     }
 
 
     protected void setearFuenteDeJLabel(JLabel label,boolean underline,int fontType,int fontSize){
 
-        Font font = new Font("StarcraftNormal", fontType, fontSize);
+        Font font = new Font("images/StarcraftNormal.ttf", fontType, fontSize);
         if(underline){
             Map attributes = font.getAttributes();
             attributes.put(TextAttribute.UNDERLINE, TextAttribute.UNDERLINE_ON);
@@ -72,8 +80,9 @@ public class VentanaGanador extends JFrame {
 
     private void configurarVentana() {
 
-        //this.setUndecorated(true);
-        //this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        this.setUndecorated(true);
+        this.getRootPane().setWindowDecorationStyle(JRootPane.NONE);
+        this.setAlwaysOnTop(true);
 
         this.setPreferredSize(new Dimension(657,364));
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
